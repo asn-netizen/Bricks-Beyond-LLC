b3c74e3a21c906e14cf89b3b6a0bcc15d11f9178
asn-netizen <ah.shahabn@gmail.com>
2026-06-30 01:51:15 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Brick & Beyond LLC website JavaScript
+
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Brick & Beyond LLC website loaded.");
+
+    const form = document.querySelector("form");
+
+    form.addEventListener("submit", function () {
+        alert("Thank you! Your request is being submitted.");
+    });
+});
